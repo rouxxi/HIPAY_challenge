ab953e7dac654ff0ed9bcee22701fa4abf6b745e
rouxxi <laskar.gwenvael@gmail.com>
2021-06-12 21:23:27 +0200

    think about remove the API REST

---
+++ backend/graphql/fakeDB/movies.js
@@ -0,0 +1,18 @@
+
+
+const movies = [
+    {
+      id: 1,
+      name: "Forrest Gump",
+      year: 2015,
+      director: 2,
+    },
+    {
+      id: 2,
+      name: "Le Parrain",
+      year: 2013,
+      director: 1
+    },
+  ];
+
+module.exports = movies;
---
+++ backend/graphql/schema/typeDefs.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+
+  type Movie {
+    id: ID
+    name: String!
+    year: Int!
+    director: Int!
+  }
+
+  type Director {
+    id: ID
+    firstname: String!
+    lastname: String!
+  }
+
+  #Query
+  type Query {
+    getAllMovies: [Movie!]
+    getMoviesByName(name:String!): [Movie!]
+  }
+
+  # Mutation
+`;
+
+module.exports = typeDefs;
