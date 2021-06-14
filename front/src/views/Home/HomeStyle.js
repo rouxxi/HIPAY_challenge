a0dfc21a382c33ae5710c31e58d7d8126ba3ed83
rouxxi <laskar.gwenvael@gmail.com>
2021-06-14 15:33:08 +0200

    add a title in Home

--- front/src/views/Home/HomeStyle.js
+++ front/src/views/Home/HomeStyle.js
@@ -2,8 +2,10 @@ import { makeStyles } from "@material-ui/core";
 
 const HomeStyle = makeStyles( {
     main: {
-        padding: '110px 0 0 0'
-    }
+        padding: '110px 0 0 0',
+        color: 'white',
+        textAlign: 'center',
+    },
 })
 
 export default HomeStyle;
