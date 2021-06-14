5a1d45f2e8b0d5bc517e6a9c2baf4f1103ad55bf
rouxxi <laskar.gwenvael@gmail.com>
2021-06-14 12:38:25 +0200

    the query work well but the archit is bad => lot of render

--- front/src/components/AllMovieList/AllMovieList.jsx
+++ front/src/components/AllMovieList/AllMovieList.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AllMovieListStyle from './AllMovieListStyle';
 import { ListItem, ListItemAvatar, List, Avatar, ListItemText, Divider } from '@material-ui/core';
 import LocalMoviesIcon from '@material-ui/icons/LocalMovies';
+import {useLazyQuery, gql} from '@apollo/client';
+
+
 
 
 function AllMovieList(props) {
-    const {data} = props;
+    const {allMovie, allView, researchField} = props;
+
+    const FETCH_MOVIES_BY_NAME = gql`
+        query ($researchInField:String!)  { 
+         getMoviesByName(name: $researchInField) {
+              name
+          }
+        }
+    `
+    const [getDataByName, {data}] = useLazyQuery(FETCH_MOVIES_BY_NAME, {variables: { researchInField: researchField }});
+
     const classes = AllMovieListStyle();
-    
+    useEffect(()=> {
+        if (!allView) {
+            getDataByName(researchField)
+        }
+        if (allView) {
+
+        }
+    }, [allView])
     return (
         <div className={classes.main}>
+            
             <List className={classes.root}>
-                {data && data.getAllMovies && data.getAllMovies.map((movie) => {
+                {data && data.getMoviesByName &&  data.getMoviesByName.length >=1? 
+                data.getMoviesByName.map((movie) => {
                     return <div key={movie.id}>
                          <ListItem>
                     <ListItemAvatar>
@@ -24,7 +46,26 @@ function AllMovieList(props) {
                 </ListItem>
                  <Divider/>
                  </div>
-                })}
+                })
+                : allMovie && allMovie.getAllMovies ? 
+                
+                allMovie.getAllMovies.map((movie) => {
+                    return <div key={movie.id}>
+                         <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <LocalMoviesIcon />
+                        </Avatar>
+                    </ListItemAvatar>
+                        <ListItemText primary={movie.name} secondary={`${movie.director}, ${movie.year}`} />
+                   
+                </ListItem>
+                 <Divider/>
+                 </div>
+                })
+                : <p>No match movie in movie list</p>
+            
+            }
                
           </List>
         </div>
--- front/src/views/Research/Research.jsx
+++ front/src/views/Research/Research.jsx
@@ -1,10 +1,10 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
 import AllMovieList from '../../components/AllMovieList/AllMovieList';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import ResearchStyle from './ResearchStyle';
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery ,gql } from '@apollo/client';
 
 const FETCH_ALL_MOVIES = gql`
    query { 
@@ -13,25 +13,33 @@ const FETCH_ALL_MOVIES = gql`
             image
             name
             year
-            
+
         }
 }
 `
+
 function Research() {
     const [allView, setAllView] = useState(true);
+
     const [researchField, setResearchField] = useState('');
 
-    const { data} = useQuery(FETCH_ALL_MOVIES);
+    const {data} = useQuery(FETCH_ALL_MOVIES);
+
 
     const classes = ResearchStyle();
+
+    const handleResearch = () => {
+        setAllView(!allView)
+    }   
+
     return (
         <div className={classes.main}>
             <Typography  className={classes.title} variant='h3'>Find the chosen One !</Typography>
             <SearchBar researchField={researchField} setResearchField={setResearchField} />
-            <Button className={classes.button}  color='inherit' >
+            <Button className={classes.button}  color='inherit' onClick={()=> handleResearch()}>
                 Valid Research
             </Button>
-            <AllMovieList data={data}/>
+            <AllMovieList allView={allView} allMovie={data} researchField={researchField} />
         </div>
     )
 }
